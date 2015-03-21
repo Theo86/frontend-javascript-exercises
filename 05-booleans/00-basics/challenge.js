c5918f26452931bf903ba2aec662ddd442fdc02e
Theodore Ford Webb <pfordwebb@gmail.com>
2015-03-21 10:11:58 -0400

    05-booleans

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,31 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+	return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+	return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	return lower < middle && middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
+	
+	
+//not between 10 and 20, it also can't be 10 or 20
+return (!(number >= 10 && number <= 20) &&
+	//not between 42 and 75, it also can't be 75. 42 is allowed
+          !(number > 42 && number <= 75) &&
+          //not between 1 and 6. 1 and 6 are allowed
+          !(number > 1 && number < 6));
 
+	// return ((number < 10 && number > 20) &&
+	// 	   (number <= 42 && number > 75) &&
+	//        (number <= 1 || number >= 6));
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+	return (((name == 'NYTimes' ) || (name == 'LATimes')) &&
+		   (price >= 1));
 };
