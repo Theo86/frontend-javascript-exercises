630bc34419f49f020230d11a193e9a772e426ef8
Theodore Ford Webb <pfordwebb@gmail.com>
2015-03-24 10:41:03 -0400

    01_Array_Functions

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,16 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(Argarray){
+	var returnarray = new Array;
+	returnarray = Argarray.reverse();
+	returnarray.unshift(1);
+	return returnarray;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(Argarray){
+	var tarray = new Array;
+	tarray = Argarray.join('+');
+	return tarray;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(Argarray){
+	return Argarray.length+1;
+};
