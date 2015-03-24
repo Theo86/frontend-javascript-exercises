ca53b6bf1df78284b657622e81f33fa1317e9745
Theodore Ford Webb <pfordwebb@gmail.com>
2015-03-24 09:42:10 -0400

    00_Array_Definition

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,12 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(N1,N2,N3,N4){
+	var array = [N1,N2,N3,N4]
+	return array;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array){
+	var first = 0, last = array.length-1;
+	//var localArray = [];
+	//localArray.push (Array[0]);
+	//localArray.push (Array[last]);
+	return Array(array[first],array[last]);
+};
