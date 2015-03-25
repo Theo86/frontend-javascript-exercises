5501f851f7a7466b3ed32f65ce4e001f5392b164
Theodore Ford Webb <pfordwebb@gmail.com>
2015-03-25 14:51:45 -0400

    02_Grocery_List

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,12 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item,grocerylist){
+	if (grocerylist.indexOf(item) == -1) {
+		grocerylist.push(item);
+	}
+	return grocerylist; 
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(list){
+	list.sort();
+	list.reverse();
+	return list;
+};
