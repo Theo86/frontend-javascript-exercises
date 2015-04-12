c1eef0a5bc1d5d12b936c3370a237de03737251e
Theodore Ford Webb <pfordwebb@gmail.com>
2015-04-12 12:58:19 -0400

    /00-looping-over-arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,24 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(NumberstoAdd) {
+	var sum = 0;
+	for (var counter = 0; counter < NumberstoAdd.length; counter++)
+	{
+		sum = sum + NumberstoAdd[counter]
+	};
+	return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	var output = inputString.split(',');
+	for (var i = 0; i < output.length; i++) {
+		output[i] = output[i].toLowerCase();
+	}
+	return output;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(arrayOfData) { 
+	var output = [];
+	for (var i = 0; i < arrayOfData.length; i++) {
+		output.push(i + " is " + arrayOfData[i]);
+	}
+	return output;
+};;
