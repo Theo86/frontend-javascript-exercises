24560e9004b7a67ed469288ba74f898d9ddb35cf
Theodore Ford Webb <pfordwebb@gmail.com>
2015-04-21 13:36:51 -0400

    03-while-loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,15 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn,actionFn){
+	while (conditionalFn()){
+		actionFn();
+	};
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(A){
+	var counter = 0, total = 0;
+	while (counter < A.length){
+		total = total + A[counter];
+		counter++;
+	};
+	return total;
+};
 
